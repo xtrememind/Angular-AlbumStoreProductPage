7cf1f7493be6770fdb0bbd5507a787dc7e788156
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-17 17:43:56 -0500

    Part 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+import {Album} from './album';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class ProductService {
   private _albumUrl: string = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum (id: number){
-    return this._http.get(this._albumUrl).map((response)=>response.json());
+  getAlbum (id: number):Observable<Album> {
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
+    
   }
 
 }
