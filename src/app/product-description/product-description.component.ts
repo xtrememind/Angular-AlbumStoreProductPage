3ad652be84f4032e55fd16d00159ccfdfd542358
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-17 17:20:07 -0500

    part 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -12,8 +12,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(
-      (response)=> this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe((response)=> this.albumInfo = response);
   }
 
 }
