7e44507476f83b3aade6b2dcc6f161473a38e5d6
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-17 17:21:53 -0500

    part 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,12 +7,12 @@ import {ProductService} from '../product.service'
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  private albumInfo;
+  albumInfo;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response)=> this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response=> this.albumInfo = response);
   }
 
 }
