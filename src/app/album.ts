51ee1c840b1cb9126b06fc05a4cd7b69df0d05ba
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-17 17:54:55 -0500

    Part 4

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,8 @@
 import {Track} from './track';
 
 export interface Album {
-    name: string,
-    releaseDate: string,
-    coverImage: string,
+    name: string;
+    releaseDate: string;
+    coverImage: string;
     tracks: Track[]
 };
