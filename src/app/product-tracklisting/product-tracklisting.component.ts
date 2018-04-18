e2e667446a5a57d2759c5d668d2ff52ed1bed7c4
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-17 21:33:24 -0500

    Part 5

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Track} from '../track';
+import {Album} from '../album';
 import {ProductService} from '../product.service';
 
 @Component({
@@ -8,7 +8,7 @@ import {ProductService} from '../product.service';
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-  albumInfo: Track;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
