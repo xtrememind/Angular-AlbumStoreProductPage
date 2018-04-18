70b018a74f0fc645cadd33f2fe61238ed8f96a81
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-04-17 22:49:06 -0500

    Part 6

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import {ProductService} from '../product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  private products: Product[];
+  products: Product[];
 
   constructor(private _productService: ProductService) { }
 
